2b1545834aaa13d17cd0fae593a398a94d5b52cf
Mengyuan <imli.mengyuan@gmail.com>
2019-08-25 23:59:58 -0400

    first option completed

--- js/code.js
+++ js/code.js
@@ -1,63 +1,68 @@
 (function ($) {
     $(document).ready(function() {
-        let inputMoney = $("CURR_INPUT");
-        let answerMoney1 = $("CURR_ANSWER1");
-        let answerMoney2 = $("CURR_ANSWER2");
-        let answerMoney3 = $("CURR_ANSWER3");
-        let answerMoney4 = $("CURR_ANSWER4");
+        let inputMoney = $('#CURR_INPUT');
+        let answerMoney1 = $('#CURR_ANSWER1');
+        let answerMoney2 = $('#CURR_ANSWER2');
+        let answerMoney3 = $('#CURR_ANSWER3');
+        let answerMoney4 = $('#CURR_ANSWER4');
         let API_ENDPOINT = "https://api.exchangeratesapi.io/latest";
        
-        // ???currOption多个选项,怎么锁定,?base=undefined
+        // ???currOption多个选项，造个函数？
         function getQueryString(){
-            return "?base="+$("#CURR_FR option:selected").val();
+            return "?base="+$('#CURR_FR option:selected').val();
         }
         
         // 实时监听
         inputMoney.on('input propertychange',function(){
-            fetchExchangeRate();
+            fetchExchangeRate('CURR_TO1',answerMoney1);
         })
-        
-        $("#CURR_FR").on('change',function(){
+
+        // need fix 
+        $("#CURR_FR").on('change', function() {
             fetchExchangeRate();
-        })
+          })
         
+        //   options
+        // 
+        fetchExchangeRate('CURR_TO1',answerMoney1);
         $("#CURR_TO1").on('change',function(){
-            fetchExchangeRate();
+            console.log(4);
+            fetchExchangeRate('CURR_TO1',answerMoney1);
         })
-        
-        fetchExchangeRate();
-        
-        // 获取数据
-        // 先做第一个成功的，后转function
-        function fetchExchangeRate(){
+           
+    
+    
+       
+        //  answerTO 问题
+        // opption 选中值
+        function fetchExchangeRate(currTo,answerTO){
+            console.log(currTo);
             let country = getQueryString();
-            alert(API_ENDPOINT+country);
             
             $.getJSON(API_ENDPOINT+country,function(data){
-                try{
-                    let currToSelect = $("#CURR_TO1 option:selected").val();
-                    var initCurrVal = function() {
-                        answerMoney1.html(numeral(100.0 * data.rates.currToSelect).format("0,0.00[0]"));
+                   
+                    // let currToSelect = $("#"+currTo+" option:selected").text();
+                    let currToSelect = $("#"+currTo).find("option:selected").text();
+                    console.log(currToSelect);
+                    // JSON objec
+                    let currFrom = parseFloat(inputMoney.val());
+                    console.log(inputMoney);
+                    // alert(currFrom);
+                    if(isNaN(currFrom)){
+                        let value = 100.0 * (data.rates[currToSelect]);
+                        answerMoney1.html(value.toFixed(2));
                     }
-                    if(inputMoney.value.length == 0){
-                        initCurrVal();
-                        return;
+                    else{
+
+                        answerMoney1.html((currFrom * data.rates[currToSelect]).toFixed(2));       
                     }
-                    let currFrVal = parseFloat(inputMoney.value);
-                    answerMoney1.html(numeral(currFrVal * data.rates.currToSelect).format("0,0.00[0]"));
-    
-                }
-                catch(e){
-                    initCurrVal();
-                    alert("Please enter a number in the Amount field.");
-                }
-                
         
-            });            
+            }); 
             
         }
         // on?
         // 获取具体汇率？
+        console.clear();
     
     });
 })(jQuery);
